512f02e4ada2757a61d9dba11480fb3baa86add5
YUXI <daiyuxilinda@163.com>
2020-02-05 12:25:14 +0000

    Changed.

--- backend.js
+++ backend.js
@@ -1,5 +1,11 @@
 function purchase() {
+    window.location.herf="./order.html";
+
 }
 
 function order() {
+    city 
+document.getElementById("city").nodeValue;
+
+    alert("we will ship to"+city);
 }
